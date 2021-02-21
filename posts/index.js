8c5e700df25b13651953504bd1665ca18b4be60a
Paolo Mosconi <pmosconi@gmail.com>
2021-02-21 17:03:53 +0100

    routing rules

--- posts/index.js
+++ posts/index.js
@@ -14,7 +14,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = {
@@ -33,6 +33,5 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('v55');
   console.log('Listening on 4000');
 });
